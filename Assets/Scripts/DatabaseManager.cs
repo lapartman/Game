791ff50204c80fc4c8f5d87676b78bd15f6cc1c1
lapartman <lapartman997@gmail.com>
2019-04-23 21:40:51 +0200

    Basic database connection

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private string connectionString;
+
+    void Start()
+    {
+        connectionString = $"URI=file:{Application.dataPath}/Database/highscore.sqlite";
+        GetScores();
+    }
+
+    private void GetScores()
+    {
+        string sqlQuery = "SELECT name, score FROM player INNER JOIN scores ON player.id = scores.playerid";
+        using (SqliteConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+            using (SqliteCommand dbCommand = new SqliteCommand(sqlQuery, dbConnection))
+            {
+                using (SqliteDataReader reader = dbCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log($"{reader.GetValue(0)}\n{reader.GetValue(1)}");
+                    }
+                    reader.Close();
+                }
+            }
+            dbConnection.Close();
+        }
+    }
+}
